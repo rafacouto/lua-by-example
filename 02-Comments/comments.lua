e48b84095cfa81821c1fbca55fc9e8927319ae32
Rafa Couto <caligari@treboada.net>
2015-12-01 20:34:35 +0100

    Comments.

---
+++ 02-Comments/comments.lua
@@ -0,0 +1,20 @@
+#!/usr/bin/env lua
+-- The first line starting with # is treated as a comment
+
+-- Comments usually start with a doublehyphen
+-- print("This line is not exectuded")
+
+--[[ 
+Use square brackets to 
+extend the comment along several lines
+and remember to close the square brackets
+]]
+
+--[=====[ 
+[[[[ square brackets  ]]]]
+If you have a special need of escaping the 
+multine comment... you can extend like here
+]=====]
+
+
+
